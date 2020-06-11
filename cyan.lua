79a93682d2e840db9cca2c5949b0159966dbb329
BrightYC <sawerty222@gmail.com>
2020-06-11 09:49:00 +0300

    Update cyan.lua

--- cyan.lua
+++ cyan.lua
@@ -1,11 +1,11 @@
-local bootFiles, bootCandidates, key, Unicode, Computer, invoke, selectedElementsLine, centerY, users, requestUserPressOnBoot, userChecked, width, height, lines, eeprom, gpu, gpuAddress, eepromData, eepromAddress = {"/init.lua", "/OS.lua", "/boot.lua"}, {}, {}, unicode, computer, component.invoke
+local bootFiles, bootCandidates, key, Unicode, Computer, invoke, selectedElementsLine, centerY, users, requestUserPressOnBoot, userChecked, width, height, lines, eeprom, gpu, screen, gpuAddress, eepromData, eepromAddress = {"/init.lua", "/OS.lua", "/boot.lua"}, {}, {}, unicode, computer, component.invoke
 
 local function pullSignal(timeout)
     local signal = {Computer.pullSignal(timeout or math.huge)}
     signal[1] = signal[1] or ""
 
     if #signal > 0 and users.n > 0 and ((signal[1]:match"key" and not users[signal[5]]) or signal[1]:match"cl" and not users[signal[4]]) then
-        return {""}
+        return table.unpack(signal)
     end
 
     key[signal[4] or ""] = signal[1] == "key_down" and 1
@@ -69,10 +69,10 @@ local function proxy(componentType)
 end
 
 local function configureGPU()
-    gpu = proxy"gp"
+    gpu, screen = proxy"gp", component.list"sc"()
 
-    if gpu and component.list"sc"() then
-        gpu.bind((component.list"sc"()))
+    if gpu and screen then
+        gpu.bind((screen))
         gpu.setPaletteColor(9, 0x002b36)
         gpu.setPaletteColor(11, 0x8cb9c5)
         gpuAddress = gpu.address
@@ -84,7 +84,11 @@ end
 
 eeprom = proxy"pr"
 eepromData = eeprom.getData()
+users = select(2, pcall(load("return " .. (eepromData:match"#(.+)#" or "{}"))))
+requestUserPressOnBoot = eepromData:match"*"
 configureGPU()
+arr2a_arr(users)
+arr2a_arr(bootFiles)
 
 function component.invoke(address, method, ...)
     if address == eepromAddress then
@@ -103,14 +107,6 @@ end
 
 Computer.setBootAddress = eeprom.getData
 Computer.getBootAddress = eeprom.setData
-
-users = select(2, pcall(load("return " .. (eepromData:match"#(.+)#" or "{}"))))
-requestUserPressOnBoot = eepromData:match"*"
-users.n = #users
-arr2a_arr(users)
-arr2a_arr(bootFiles)
-
-configureGPU()
 
 local function set(x, y, string, background, foreground)
     gpu.setBackground(background or 0x002b36)
@@ -231,38 +227,53 @@ local function print(...)
 end
 
 local function bootPreview(drive, booting)
-    local address = cutText(drive[3], booting and 36 or 6)
-    return drive[4] and ("Boot%s %s from %s (%s)")
-        :format(
-            booting and "ing" or "",
-            drive[4],
-            drive[2],
-            address
-        )
-    or ("Boot from %s (%s) is not available")
-        :format(
-            drive[2],
-            address
-        )
+    if drive[6] then
+        return ("Boot%s %s from %s")
+            :format(
+                booting and "ing" or "",
+                drive[3],
+                drive[2]
+            )
+    else
+        local address = cutText(drive[3], booting and 36 or 6)
+        return drive[4] and ("Boot%s %s from %s (%s)")
+            :format(
+                booting and "ing" or "",
+                drive[4],
+                drive[2],
+                address
+            )
+        or  ("Boot from %s (%s) is not available")
+            :format(
+                drive[2],
+                address
+            )
+    end
 end
 
 local function addCandidate(address)
-    local proxy = component.proxy(address)
-
-    if proxy and proxy.spaceTotal and address ~= Computer.tmpAddress() then
+    if address:match("http") then
         bootCandidates[#bootCandidates + 1] = {
-            proxy, proxy.getLabel() or "N/A", address, F, ("Disk usage %s%% / %s / %s")
-                :format(
-                    math.floor(proxy.spaceUsed() / (proxy.spaceTotal() / 100)),
-                    proxy.readOnly() and "Read only" or "Read & Write",
-                    proxy.spaceTotal() < 2 ^ 20 and "FDD" or proxy.spaceTotal() < 2 ^ 20 * 12 and "HDD" or "RAID"
-                )
-        }
+            F, "Netboot", address, F, "", 1
+        } 
+    else
+        local proxy = component.proxy(address)
 
-        for i = 1, #bootFiles do
-            if proxy.exists(bootFiles[i]) then
-                bootCandidates[#bootCandidates][4] = bootFiles[i]
-                break
+        if proxy and proxy.spaceTotal and address ~= Computer.tmpAddress() then
+            bootCandidates[#bootCandidates + 1] = {
+                proxy, proxy.getLabel() or "N/A", address, F, ("Disk usage %s%% / %s / %s")
+                    :format(
+                        math.floor(proxy.spaceUsed() / (proxy.spaceTotal() / 100)),
+                        proxy.readOnly() and "Read only" or "Read & Write",
+                        proxy.spaceTotal() < 2 ^ 20 and "FDD" or proxy.spaceTotal() < 2 ^ 20 * 12 and "HDD" or "RAID"
+                    )
+            }
+
+            for i = 1, #bootFiles do
+                if proxy.exists(bootFiles[i]) then
+                    bootCandidates[#bootCandidates][4] = bootFiles[i]
+                    break
+                end
             end
         end
     end
@@ -270,15 +281,14 @@ end
 
 local function updateCandidates()
     bootCandidates = {}
-    addCandidate(getData())
+    addCandidate(eeprom.getData())
 
     for filesystem in pairs(component.list"f") do
-        addCandidate(getData() ~= filesystem and filesystem or "")
+        addCandidate(eeprom.getData() ~= filesystem and filesystem or "")
     end
 end
 
 local function boot(drive)
-    stdout(drive)
     if drive[4] then
         local handle, data, chunk, success, err, boot = drive[1].open(drive[4], "r"), ""
 
@@ -294,8 +304,8 @@ local function boot(drive)
 
         boot = function()
             status(bootPreview(drive, 1), F, .5, F, F, 1)
-            if getData() ~= drive[3] then
-                setData(drive[3])
+            if eeprom.getData() ~= drive[3] then
+                eeprom.setData(drive[3])
             end
             success, err = execute(data, "=" .. drive[4])
             status(err, [[¯\_(ツ)_/¯]], math.huge, 0, Computer.shutdown, 1)
@@ -324,125 +334,14 @@ local function bootLoader()
         goto LOOP
     end
 
-    do 
-        local function createElements()
-
-        options = createElements({
-            {t = "Power off", a = function() Computer.shutdown() end},
-            {t = "Lua", a = function()
-                clear()
-
-                ::LOOP::
-                    data = input("> ", 1, height, F, data)
-
-                    if data then
-                        print("> " .. data)
-                        set(1, height, ">")
-                        print(select(2, execute(data, "=stdin", env)))
-                        goto LOOP
-                    end
-                draw(F, F, 1, 1)
-            end},
-        }, centerY + 2, 1, function()
-            selectedElementsLine = drives
-            draw(1, 1, F, F)
-        end)
-
-        options.e[#options.e + 1] = proxy"et" and {t = "Netboot", a = function()
-            url, data = input("URL: ", F, centerY + 7, 1), ""
-
-            if #url > 0 then
-                handle, chunk = proxy"et".request(url), ""
-
-                if handle then
-                    status("Downloading " .. url .. "...")
-                    ::LOOP::
-
-                    chunk = handle.read()
-
-                    if chunk then
-                        data = data .. chunk
-                        goto LOOP
-                    end
-
-                    handle.close()
-                    status(select(2, execute(data, "=netboot")) or "is empty", "Netboot:", math.huge, 0)
-                else
-                    status("Invalid URL", "Netboot:", math.huge, 0)
-                end
-            end
-
-            draw(F, F, 1, 1)
-        end}
-
-        if #bootCandidates > 0 then
-            correction = #options.e + 1
-            drives = createElements({}, centerY - 2, 2, function()
-                selectedElementsLine = options
-                draw(F, F, 1, 1)
-            end, function(SELF)
-                drive = bootCandidates[SELF.s]
-                proxy = drive[1]
-                spaceTotal = proxy.spaceTotal()
-                readOnly = proxy.isReadOnly()
-                fill(1, centerY + 5, width, 3, " ")
-                centrizedSet(centerY + 5, bootPreview(drive), F, 0xFFFFFF)
-
-                for i = correction, #options.e do
-                    options.e[i] = F
-                end
-
-                if readOnly then
-                    options.s = options.s > #options.e and #options.e or options.s
-                else
-                    options.e[correction] = {t = "Rename", a = function()
-                        newLabel = input("New label: ", F, centerY + 7, 1)
-
-                        if #newLabel > 0 then
-                            pcall(proxy.setLabel, newLabel)
-                            drive[2] = cutText(newLabel, 16)
-                            drives.e[SELF.s].t = cutText(newLabel, 6)
-                        end
-
-                        drives:d(1, 1)
-                        options:d()
-                    end}
-                    options.e[#options.e + 1] = {t = "Format", a = function() drive[4] = F proxy.remove("/") drives:d(1, 1) options:d() end}
-                end
-
-                options:d(1, 1)
-            end)
-
-            for i = 1, #bootCandidates do
-                drives.e[i] = {t = cutText(bootCandidates[i][2], 6), a = function(SELF)
-                    boot(bootCandidates[SELF.s])
-                end}
-            end
-        else
-            options.y = centerY
-            options.b = 2
-        end
-
-        draw = function(optionsWithoutBorder, optionsWithoutSelect, drivesWithoutBorder, drivesWithoutSelect)
-            clear()
-            if drives then
-                drives:d(drivesWithoutBorder, drivesWithoutSelect)
-                options:d(optionsWithoutBorder, optionsWithoutSelect)
-            else
-                centrizedSet(centerY + 4, "No drives available", 0x002b36, 0xFFFFFF)
-                options:d()
-            end
-
-            centrizedSet(height, "Use ← ↑ → key to move cursor; Enter to do action; CTRL+D to shutdown")
-        end
-
-        draw(1, 1)
+    do
+        
     end
 
     ::LOOP::
         signalType, _, _, code = pullSignal()
 
-        if signalType == "key_down" and gpu and screen then
+        if signalType == "key_down" and gpu then
             if code == 200 then -- Up
                 selectedElementsLine.k()
             elseif code == 208 then -- Down
